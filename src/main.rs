a884711d52fc21ef262e582323131184cce6712d
muzkii <andriyo.averill@ui.ac.id>
2025-05-16 13:59:08 +0700

    Initial subscriber code

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, MessageHandler, HandleError};
+use std::{thread, time};
+
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String
+}
+
+
+pub struct UserCreatedHandler;
+
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+
+    fn get_handler_action(&self) -> String {
+        "process_user_created".to_string()
+    }
+
+    fn handle(&self, message: Box<UserCreatedEventMessage>
+    ) -> Result<(), HandleError> {
+        let _ten_millis = time::Duration::from_millis(1000);
+        let _now = time::Instant::now();
+        
+        // thread::sleep(_ten_millis);
+     
+        println!("In Andriyo Averill's Computer [2306172325]. Message received: {:?}", message);
+        Ok(())
+    }
+}
+
+
+fn main() {
+    let listener = CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+    _ = listener.listen("user_created".to_owned(), UserCreatedHandler{}, crosstown_bus::QueueProperties { auto_delete: false, durable: false, use_dead_letter: true });
+
+
+    loop {
+    }
+}
